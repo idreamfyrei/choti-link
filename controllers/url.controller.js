93dae418f105a77fedf3f8ead68e9f54c77b1bdd
idreamfyrei <saumya.x3@gmail.com>
2025-05-20 01:57:27 +0530

    add: url redirect and analytics

--- controllers/url.controller.js
+++ controllers/url.controller.js
@@ -1,18 +1,35 @@
 import URL from "../models/url.model.js";
 import { nanoid } from "nanoid";
 
-async function handleGetUrlShortener(req,res){
-    const body = req.body;
-    if( !body.url ) return res.status(400).json({error:'url is required'});
-    const shortID = nanoid(8);
+async function handleGetUrlShortener(req, res) {
+  const body = req.body;
+  if (!body.url) return res.status(400).json({ error: "url is required" });
+  const shortID = nanoid(8);
 
-    await URL.create({
-        shortId: shortID,
-        redirectURL: body.url,
-        visitHistory: [],
-    });
+  await URL.create({
+    shortID: shortID,
+    redirectURL: body.url,
+    visitHistory: [],
+  });
 
-    return res.json({id: shortID });
+  return res.json({ id: shortID });
 }
 
-export { handleGetUrlShortener };
+async function handleRedirectURL(req, res) {
+  const shortID = req.params.shortID; 
+  console.log(shortID);
+  //* const entry = await URL.findOneAndUpdate({ shortID }, { $push: {visitHistory: {timestamp: Date.now()}}});
+  const entry = await URL.findOne({ shortID });
+  console.log(entry);
+  entry.visitHistory.push({timestamp: Date.now()});
+  await entry.save();
+  return res.redirect(entry.redirectURL);
+}
+
+async function handleAnalytics(req, res) {
+    const shortID = req.params.shortID;
+    const entry = await URL.findOne({ shortID });
+    return res.json({totalClicks: entry.visitHistory.length, analytics: entry.visitHistory})
+}
+
+export { handleGetUrlShortener, handleRedirectURL, handleAnalytics };
--- routes/url.route.js
+++ routes/url.route.js
@@ -1,9 +1,13 @@
 import express from 'express';
-import {handleGetUrlShortener} from '../controllers/url.controller.js';
+import { handleGetUrlShortener, handleRedirectURL, handleAnalytics } from '../controllers/url.controller.js';
 
 const router = express.Router();
 
 router.post('/', handleGetUrlShortener);
+
+router.get('/:shortID', handleRedirectURL);
+
+router.get('/analytics/:shortID', handleAnalytics);
 
 export default router;
 
